17f99eec6f2c113975b9b00239ac2d33e580f72a
Timeryan Badertdinov <timeryan2001@gmail.com>
2022-03-30 19:50:39 +0300

    Дабавил sll

--- backend/ironprocessing/ironprocessing/Program.cs
+++ backend/ironprocessing/ironprocessing/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddControllers();
 
 builder.Services.AddDbContext<DataContext>(options =>
 {
-    options.UseNpgsql(builder.Configuration.GetConnectionString("ConnectionStrings__PostgresDb"));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
--- ironprocessing/src/environments/environment.prod.ts
+++ ironprocessing/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'http://sevmetallkot.ru',
+  apiUrl: 'https://sevmetallkot.ru',
   production: true
 };
